dc8f8a2c7f64d3c5d91bed825179277e789cfcd8
knut <knut@KNUT-8>
2016-06-05 08:33:42 +0200

    Add project files.

---
+++ Bjornebo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bjornebo.Startup))]
+namespace Bjornebo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
